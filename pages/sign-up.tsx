e276be3deea5e47da5b33f3069a9841db19a0071
Ayotunde Obasa <ayobam222@gmail.com>
2022-07-03 21:04:47 +0100

    Update sign-up.tsx

--- pages/sign-up.tsx
+++ pages/sign-up.tsx
@@ -12,14 +12,14 @@ const SignUp = () => {
         <div className="flex justify-center pb-2 font-medium">
           <h2 className=" text-2xl text-pup">Signup</h2>
         </div>
-        <div className="flex flex-col gap-6">
-          {/* <div className="flex flex-col sm:gap-6 md:gap-4 gap-6"> */}
+        {/* <div className="flex flex-col gap-6"> */}
+        <div className="flex flex-col sm:gap-6 md:gap-4 gap-6">
           <Input type="text" placeholder="Username or Email" />
           <Input type="password" placeholder="Password" />
           <Input type="password" placeholder="Re-enter Password" />
         </div>
-        <div className=" px-12 pt-14">
-          {/* <div className=" px-12 sm:pt-14 md:pt-10 pt-14"> */}
+        {/* <div className=" px-12 pt-14"> */}
+        <div className=" px-12 sm:pt-14 md:pt-10 pt-14">
           <Button text="Next" onClick={signUp} />
         </div>
       </LoginFrame>
